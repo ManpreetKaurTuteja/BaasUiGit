3d6fba5a4b3be8958f25b12079163b559270c96e
Manpreet Kaur <>
2022-06-07 15:07:03 +0530

    - maven publish added -artifacts added

--- baas-core-sdk/src/main/java/com/payu/baas/coreUI/nonUI/util/JsonUtils.java
+++ baas-core-sdk/src/main/java/com/payu/baas/coreUI/nonUI/util/JsonUtils.java
@@ -2,6 +2,9 @@ package com.payu.baas.coreUI.nonUI.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+import java.io.StringReader;
 
 /*
 Converting JSON data to Gson data (models)
@@ -13,7 +16,7 @@ public class JsonUtils {
 
     @SuppressWarnings({"rawtypes", "unchecked"})
     public static Object toObject(String json, Class type) { // json string to object conversion
-        return getGson().fromJson(json, type);
+        return getGson().fromJson(json.trim(), type);
     }
 
     public static Gson getGson() {
